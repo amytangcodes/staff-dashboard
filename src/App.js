8f724b7421673b0ecf0392df778f45c929b9f346
Amy <amytang77@gmail.com>
2021-01-15 23:46:30 -0500

    add sass | get router to work for menu nav links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+
+import Homepage from "./component/Homepage";
+import Aboutpage from "./component/Aboutpage";
+import "./App.scss";
 
 class App extends Component {
   componentDidMount() {
@@ -17,20 +21,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router>
+          <Navbar />
+          <Route path="/" exact render={() => <Homepage />} />
+          <Route path="/about" exact render={() => <Aboutpage />} />
+        </Router>
       </div>
     );
   }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react'
-import { Input, Menu } from 'semantic-ui-react'
-import { NavLink, withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
 export default class MenuExampleSecondary extends Component {
-  state = { activeItem: 'home' }
+  state = { activeItem: "home" };
 
   handleItemClick = (e, { name }) => {
-    this.setState({ activeItem: name })
-  }
+    this.setState({ activeItem: name });
+  };
 
   render() {
-    const { activeItem } = this.state
+    const { activeItem } = this.state;
 
     return (
       <Menu secondary>
         <Menu.Item
-          as={NavLink} to="/"
-          name='home'
-          active={activeItem === 'home'}
+          as={NavLink}
+          to="/"
+          name="home"
+          active={activeItem === "home"}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          as={NavLink} to="/about"
-          name='about'
-          active={activeItem === 'about'}
+          as={NavLink}
+          to="/about"
+          name="about"
+          active={activeItem === "about"}
           onClick={this.handleItemClick}
         />
         <Menu.Item
-          as={NavLink} to="/profile"
-          name='my profile'
-          active={activeItem === 'profile'}
+          as={NavLink}
+          to="/profile"
+          name="my profile"
+          active={activeItem === "profile"}
           onClick={this.handleItemClick}
         />
-        </Menu.Menu>
       </Menu>
-    )
+    );
   }
 }
