4e1fcaa880f7b386a11e45c49e4442c1b7cd02b1
Amy <amytang77@gmail.com>
2021-01-16 14:39:04 -0500

    add style variables and font | clean up navbar | get nav links to work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
+import { Container } from "semantic-ui-react";
 
+import Navbar from "./component/Navbar";
 import Homepage from "./component/Homepage";
 import Aboutpage from "./component/Aboutpage";
-import "./App.scss";
+import "./styles/App.scss";
 
 class App extends Component {
   componentDidMount() {
@@ -21,11 +22,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
-          <Navbar />
-          <Route path="/" exact render={() => <Homepage />} />
-          <Route path="/about" exact render={() => <Aboutpage />} />
-        </Router>
+        <Container>
+          <Router>
+            <Navbar />
+            <Route path="/" exact render={() => <Homepage />} />
+            <Route path="/about" exact render={() => <Aboutpage />} />
+          </Router>
+        </Container>
       </div>
     );
   }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -3,38 +3,20 @@ import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
 export default class MenuExampleSecondary extends Component {
-  state = { activeItem: "home" };
-
-  handleItemClick = (e, { name }) => {
-    this.setState({ activeItem: name });
-  };
-
   render() {
-    const { activeItem } = this.state;
-
     return (
-      <Menu secondary>
-        <Menu.Item
-          as={NavLink}
-          to="/"
-          name="home"
-          active={activeItem === "home"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={NavLink}
-          to="/about"
-          name="about"
-          active={activeItem === "about"}
-          onClick={this.handleItemClick}
-        />
-        <Menu.Item
-          as={NavLink}
-          to="/profile"
-          name="my profile"
-          active={activeItem === "profile"}
-          onClick={this.handleItemClick}
-        />
+      <Menu secondary id="navbar">
+        <Menu.Item>
+          <a href="/" className="home-logo">
+            Wrapbook
+          </a>
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item as={NavLink} exact to="/" name="home" />
+          <Menu.Item as={NavLink} to="/link" name="link" />
+          <Menu.Item as={NavLink} to="/settings" name="settings" />
+          <Menu.Item as={NavLink} to="/profile" name="logout" />
+        </Menu.Menu>
       </Menu>
     );
   }
