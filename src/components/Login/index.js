ebd21d2d2f765049f6f83b1c77207a81fd5ffbe9
Amy <amytang77@gmail.com>
2021-01-21 18:48:25 -0500

    login component styling

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Login.scss";
+import SectionHeader from "../SectionHeader";
 
 const Login = () => {
   return (
     <section className="logged-out-page">
       <div className="login-container">
+        <SectionHeader
+          header="Log Into Wrapbook"
+          description="Tackle timecards, payroll, and insurance with Wrapbook, a powerful and
+          easy to use platform for managing productions."
+        />
         <div className="section-header">
           <h1 className="header">Log Into Wrapbook</h1>
           <p className="subhead">
@@ -22,7 +28,7 @@ const Login = () => {
             <input type="password" />
           </div>
           <div>
-            <button type="submit">Submit</button>
+              className="form-button"
           </div>
         </form>
       </div>
