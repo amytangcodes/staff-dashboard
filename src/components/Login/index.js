74588b9810d364e48ed91b6339fd3c375bf9fafb
Amy <amytang77@gmail.com>
2021-01-21 18:53:45 -0500

    login fixes

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,35 +5,30 @@ import SectionHeader from "../SectionHeader";
 const Login = () => {
   return (
     <div className="page-container">
-    <section className="logged-out-page">
-      <div className="login-container">
-        <SectionHeader
-          header="Log Into Wrapbook"
-          description="Tackle timecards, payroll, and insurance with Wrapbook, a powerful and
+      <section className="logged-out-page">
+        <div className="login-container">
+          <SectionHeader
+            header="Log Into Wrapbook"
+            description="Tackle timecards, payroll, and insurance with Wrapbook, a powerful and
           easy to use platform for managing productions."
-        />
-        <div className="section-header">
-          <h1 className="header">Log Into Wrapbook</h1>
-          <p className="subhead">
-            Tackle timecards, payroll, and insurance with Wrapbook, a powerful
-            and easy to use platform for managing productions.
-          </p>
+          />
+          <form className="form login-form">
+            <div className="form-group">
+              <label>Email</label>
+              <input type="text" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" />
+            </div>
+            <div>
+              <button type="submit" className="form-button">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-        <form className="form login-form">
-          <div className="form-group">
-            <label>Email</label>
-            <input type="text" />
-          </div>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password" />
-          </div>
-          <div>
-              className="form-button"
-          </div>
-        </form>
-      </div>
-    </section>
+      </section>
     </div>
   );
 };
